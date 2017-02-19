551cf55b6bb0f8551bbb5f60a8afe4af5b72a363
nikhilg18 <nikhilgolechha1@gmail.com>
2017-02-19 07:06:56 -0800

    vidc: add lazyplug hooks

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -25,6 +25,8 @@
 
 #define MAX_EVENTS 30
 
+extern void lazyplug_enter_lazy(bool enter);
+
 static int get_poll_flags(void *instance)
 {
 	struct msm_vidc_inst *inst = instance;
@@ -1280,6 +1282,8 @@ void *msm_vidc_open(int core_id, int session_type)
 		msm_vidc_debugfs_init_inst(inst, core->debugfs_root);
 
 	setup_event_queue(inst, &core->vdev[session_type].vdev);
+ 
+        lazyplug_enter_lazy(true);
 
 	return inst;
 fail_init:
@@ -1410,6 +1414,8 @@ int msm_vidc_close(void *instance)
 	msm_smem_delete_client(inst->mem_client);
 	pr_info(VIDC_DBG_TAG "Closed video instance: %p\n", VIDC_INFO, inst);
 	kfree(inst);
+
+        lazyplug_enter_lazy(false);
 
 	return 0;
 }
